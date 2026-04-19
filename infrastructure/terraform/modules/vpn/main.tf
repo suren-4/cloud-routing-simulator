d98ce678b99e5334e5a01a0ebc3648d4c9e089d3
Suren <surenkumar0404@gmail.com>
2026-04-19 22:22:26 +0530

    fix: explicit port 22 opening for EC2 instance connect SSH

--- infrastructure/terraform/modules/vpn/main.tf
+++ infrastructure/terraform/modules/vpn/main.tf
@@ -23,6 +23,12 @@ resource "aws_security_group" "vpn" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
